8f00d43303940bd03351595b385b36133a084967
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-03-18 18:30:55 +1300

    Updated Base Component

--- 00_MMF_base_v3.py
+++ 00_MMF_base_v3.py
@@ -1,14 +1,15 @@
-"""Added 01_name_not_blank_v3
+"""Added 02_ticket_loop_v4
 """
 
 # Import Statements
 
 # Functions go here
 
+
 # Check that the ticket name is not blank
 def not_blank(question):
     while True:
-        response = input(question)
+        response = input(question).title()
         if not response.isalpha():  # Ensures input contains at least 1 letter
             print("You can't leave this blank...")  # Error if not
         else:
@@ -21,10 +22,27 @@ def not_blank(question):
 # Ask user if they have used the program before and
 # show instructions if needed
 
-# Loop to get ticket details
 
-    # Get name (can't be blank)
-    name = not_blank("What's your name? ", )
+# Loop to get ticket details
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "Xxx" and count < MAX_TICKETS:
+    if MAX_TICKETS - count > 1:
+        print(f"\nYou have {MAX_TICKETS - count} seats left")
+    else:
+        print(f"\n**** You have ONLY ONE seat left! ****")
+    # Get details
+    name = not_blank("What's your name? ")
+    if name != "Xxx":
+        count += 1  # Don't want to include escape code in the count
+
+if count < MAX_TICKETS:
+    print(f"\nYou have sold {count} tickets")
+    print(f"There are still {MAX_TICKETS - count} available")
+else:
+    print("\nYou have sold all the available tickets")
     # Get age (between 12 and 130)
 
     # Calculate ticket price
