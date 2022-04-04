4868bae1bf97aec1b79f577d0cd48ce3e784fe52
jwong1109 <83558222+jwong1109@users.noreply.github.com>
2022-04-04 17:48:30 +1200

    Component 7

---
+++ 07_string_analyser_v1.py
@@ -0,0 +1,10 @@
+"""Trialling methods of extracting and testing the first character in a string
+to see if it is a number. In this program all I have done is to confirm a
+method of splitting the first character off from the rest of the string.
+
+"""
+
+my_string = "Hello world"
+
+print(f"First letter: {my_string[0]}")
+print(f"Rest of string: {my_string[1:]}")
